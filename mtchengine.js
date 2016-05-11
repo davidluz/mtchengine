61ef0eb47f63fc943ddf1ad6af857908cf1d904f
davidluz <davidmelo8@hotmail.com>
2016-05-11 17:33:03 -0300

    v 1.0

---
+++ mtchengine.js
@@ -0,0 +1,46 @@
+//====MTC Engine v1 - Maio 2016===
+
+
+//init
+$(document).ready(function() {
+		var contexto1 = new Context();
+		contexto1.viewLog();
+});
+
+
+//Logical functions
+
+// Objeto contexto - possui todos os métodos estáticos do contexto
+var Context = function (){
+
+}
+
+Context.prototype.viewLog = function(){
+	console.log("MTC V1 - 2016-05-15 - About this Project");
+	console.log("...");
+}
+
+Context.prototype.sampleStimulus = function(){
+
+}
+
+Context.prototype.comparisonStimulus = function(){
+
+}
+
+Context.prototype.timeOfSession = function(){
+
+}
+
+
+var Match = function(id,sampleStimulus,comparisonStimulus) {
+  this.id = id;
+  this.sampleStimulus = sampleStimulus;
+  this.comparisonStimulus = comparisonStimulus;
+};
+
+
+
+//Render functions
+
+//Model functions
